fe1410ac6f652bc65a9a54a15aa113253058d334
CheaFernandez <chelsea_anne_fernandez@dlsu.edu.ph>
2023-12-03 15:42:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# The program accepts three inputs from the user.
-# - n - maximum number of concurrent instances
-# - t - number of tank players in the queue
-# - h - number of healer players in the queue
-# - d - number of DPS players in the queue
-# - t1 - minimum time before an instance is finished
-# - t2 - maximum time before an instance is finished
 import process
 
 def run_simulation():
-    
     user_input = input()
     if len(user_input.split()) != 6:
         # TODO: Change this if we have to keep asking if input is invalid
